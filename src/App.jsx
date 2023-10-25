d953dff3eb2a54af40216f216e1d0fbbe3c6a006
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-10-24 22:15:22 -0700

    started on the 2nd phase of resty

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,37 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './App.scss';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
 
-class App extends React.Component {
+function App() {
+  const [data, setData] = useState(null);
+  const [requestParams, setRequestParams] = useState({});
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
-  }
-
-  callApi = (requestParams) => {
-    // mock output
-    const data = {
+  const callApi = (requestParams) => {
+    const mockData = {
       count: 2,
       results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
+        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
       ],
     };
-    this.setState({data, requestParams});
-  }
 
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div>Request Method: {this.state.requestParams.method}</div>
-        <div>URL: {this.state.requestParams.url}</div>
-        <Form handleApiCall={this.callApi} />
-        <Results data={this.state.data} />
-        <Footer />
-      </React.Fragment>
-    );
-  }
+    setData(mockData);
+    setRequestParams(requestParams);
+  };
+
+  return (
+    <>
+      <Header />
+      <div>Request Method: {requestParams.method}</div>
+      <div>URL: {requestParams.url}</div>
+      <Form handleApiCall={callApi} />
+      <Results data={data} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,11 +1,17 @@
+import React, { useState } from 'react';
 import './Form.scss';
 
 function Form(props) {
+  const [url, setUrl] = useState('');
+  const [method, setMethod] = useState('GET');
+  const [requestBody, setRequestBody] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
-      method: 'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      method: method,
+      url: url,
+      ...(requestBody && { body: JSON.parse(requestBody) }),
     };
     props.handleApiCall(formData);
   }
@@ -15,14 +21,32 @@ function Form(props) {
       <form onSubmit={handleSubmit}>
         <label>
           <span>URL: </span>
-          <input name='url' type='text' />
+          <input
+            name='url'
+            type='text'
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
           <button type="submit">GO!</button>
         </label>
         <label className="methods">
-          <span id="get">GET</span>
-          <span id="post">POST</span>
-          <span id="put">PUT</span>
-          <span id="delete">DELETE</span>
+          <select
+            value={method}
+            onChange={(e) => setMethod(e.target.value)}
+          >
+            <option value="GET">GET</option>
+            <option value="POST">POST</option>
+            <option value="PUT">PUT</option>
+            <option value="DELETE">DELETE</option>
+          </select>
+        </label>
+        <label>
+          <span>Request Body (JSON): </span>
+          <textarea
+            name='requestBody'
+            value={requestBody}
+            onChange={(e) => setRequestBody(e.target.value)}
+          />
         </label>
       </form>
     </>
